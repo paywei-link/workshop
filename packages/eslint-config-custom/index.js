b5368d36188484d7c3ba4f0cdff5ed55314ee6dc
0xgafu <0xgafu@protonmail.me>
2022-11-28 22:29:12 +0000

    Fix linter

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -7,15 +7,12 @@ module.exports = {
     "eslint:recommended",
     'plugin:tailwindcss/recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    "next",
     "turbo",
     "prettier"
   ],
 
   plugins: [
     '@typescript-eslint',
-    'react',
     'tailwindcss'
   ],
 
@@ -26,9 +23,7 @@ module.exports = {
 
   rules: {
     "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
     "curly": ["error", "multi-line"],
-    "vue/multi-word-component-names": ["off"],
     "no-console": ["off"],
     "max-len": ["warn", { ignoreComments: true }],
     "@typescript-eslint/no-explicit-any": ["off"]
